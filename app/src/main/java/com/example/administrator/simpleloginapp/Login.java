7c5443df952be9eda2d214eaaf3575592e0bbf0f
linuxalfa554 <linuxalfa554@gmail.com>
2018-05-03 10:28:22 +0800

    加入Login

--- app/src/main/java/com/example/administrator/simpleloginapp/Login.java
+++ app/src/main/java/com/example/administrator/simpleloginapp/Login.java
@@ -1,13 +1,30 @@
 package com.example.administrator.simpleloginapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
+    private static EditText username;
+    private static EditText password;
+    private static TextView attempts;
+    private static Button login_btn;
+
+    int attempt_counter =5 ;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
     }
+
+
 }
