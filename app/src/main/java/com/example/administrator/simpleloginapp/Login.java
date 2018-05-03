9362c6a55ad909ae7e4ed425f824d93a763e023b
linuxalfa554 <linuxalfa554@gmail.com>
2018-05-03 12:13:41 +0800

    加入Login

--- app/src/main/java/com/example/administrator/simpleloginapp/Login.java
+++ app/src/main/java/com/example/administrator/simpleloginapp/Login.java
@@ -10,6 +10,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
+
     private static EditText username;
     private static EditText password;
     private static TextView attempts;
@@ -18,12 +19,44 @@ public class Login extends AppCompatActivity {
     int attempt_counter =5 ;
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        LoginBtton();
+    }
 
+    public void LoginBtton(){
+        username = (EditText)findViewById(R.id.editText_name);
+        password = (EditText)findViewById(R.id.editText_password);
+        attempts = (TextView)findViewById(R.id.time_c);
+        login_btn= (Button)findViewById(R.id.button_login);
+
+        attempts.setText(Integer.toString(attempt_counter));
+        login_btn.setOnClickListener(
+                new View.OnClickListener(){
+                    @Override
+                    public void onClick (View v){
+                        if (username.getText().toString().equals("user") &&
+                                password.getText().toString().equals("pass")){
+                            Toast.makeText(Login.this,"User OK",Toast.LENGTH_SHORT).show();
+                            Intent intent=new Intent(".User");
+                            startActivity(intent);
+
+                        }else {
+                            Toast.makeText(Login.this,"NO ",Toast.LENGTH_SHORT).show();
+                            attempt_counter--;
+                            attempts.setText(Integer.toString(attempt_counter));
+                            if (attempt_counter==0){
+                                login_btn.setEnabled(false);
+                            }
+                        }
+                    }
+                }
+        );
     }
 
 
